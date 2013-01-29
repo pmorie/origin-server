696c0553f82efe23b34215ff0ff8397e6d7b6e3c
Paul Morie <pmorie@gmail.com>
2013-01-29 15:33:16 -0500

    wip

---
+++ node/lib/openshift-origin-node/utils/sdk.rb
@@ -0,0 +1,15 @@
+module OpenShift
+  module Utils
+    class Sdk
+      MARKER = 'CARTRIDGE_VERSION_2'
+
+      def self.is_new_sdk_app(gear_home)
+        File.exists?(File.join(gear_home, '.env', MARKER))
+      end
+
+      def self.mark_new_sdk_app(gear_home) 
+        FileUtils.touch(File.join(gear_home, '.env', MARKER))
+      end
+    end
+  end
+end
