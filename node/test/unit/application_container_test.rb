30b7c6ec0bd855dbf923eeca71cb6cd958740876
Andy Goldstein <andy.goldstein@gmail.com>
2013-08-19 13:52:00 -0400

    Build & deployment improvements
    Add support for OPENSHIFT_AUTO_DEPLOY to enable/disable automatic
    deployments after building
    
    Add support for OPENSHIFT_DEPLOYMENT_TYPE - disallow git push if set
    to binary
    
    Add support for deploying branches other than master
    
    Add "gear deploy <mybranch>"
    
    Add support for preserving previous deployments (to support rollback)
    
    Switch to using date/time based deployment directories
    
    Add initial support for 'gear prepare FILE'
    
    Add support for distributing a deployment to child gears
    
    Add creation of app-deployments and app-archives directories to app
    setup
    
    Add 'activate' deployment action
    
    Add writing of deployment metadata w/deployment id and state
    
    Add support for app-dependencies to be unique per deployment
    
    Update cartridges to support app-root/runtime/dependencies
    
    Add deployment rollback support
    
    Add PathUtils#oo_lchown to change symlink ownership of repo and
    dependencies symlinks
    
    Add old deployment directory pruning
    
    Reduce list of dirs to sync in sdk#sync_gear_repos
    
    Add copying of dependencies between deployments (to save on build
    time) unless force_clean_build is specified
    
    Move app dependencies from the previous deployment to the new build and
    delete the previous deployment prior to performing the new build if the
    app isn't configured to allow rollbacks (thus avoiding having 2 copies
    of the dependencies and repo for a brief period of time)
    
    Move the haproxy cartridge's gear registry to the runtime
    platform
    
    Add oo-ssh to use for rsync/ssh as the gear user
    
    Add "primary" endpoint option that indicates that the endpoint should
    be load balanced if the app is scalable (along with
    OPENSHIFT_LOAD_BALANCER_PORT env var)
    
    Add initial functional tests for build & deployment improvements
    
    Add basic scaling test that:
    - creates an app
    - validates the gear registry contents
    - verifies the <title> retrieved from /
    - scales up to 2
    - verifies <title> from both gears
    - modifies the <title> and pushes the change
    - verifies <title> was updated on both gears
    - scales to 3
    - verifies <title> is still updated on all 3
    - issues rollback
    - verifies <title> matches rolled back version
    
    Update all jenkins shell command files to create deployment dirs and
    sync the repo and dependencies back to the upstream gear
    
    Add "gear archive" command to create binary artifact from current
    deployment on a gear
    
    Add primary:true option to 8080 endpoint for scalable carts
    
    Don't stop/start web proxy cart during pre-receive, activation
    
    Use the current deployment dir instead of creating a new one if
    hot_deploy is enabled
    
    Don't stop the web proxy cart on rollback
    
    Add 'gear deployments' command

--- node/test/unit/application_container_test.rb
+++ node/test/unit/application_container_test.rb
@@ -91,6 +91,7 @@ class ApplicationContainerTest < OpenShift::NodeTestCase
             Private-Port-Name: EXAMPLE_PORT1
             Private-Port:      8080
             Public-Port-Name:  EXAMPLE_PUBLIC_PORT1
+            Options:           { primary: true }
             Mappings:
               - Frontend:      "/front1a"
                 Backend:       "/back1a"
@@ -153,7 +154,11 @@ class ApplicationContainerTest < OpenShift::NodeTestCase
     proxy.expects(:add).with(@user_uid.to_i, "127.0.0.1", 8082).returns(@ports_begin+2)
     proxy.expects(:add).with(@user_uid.to_i, "127.0.0.2", 9090).returns(@ports_begin+3)
 
-    @container.expects(:add_env_var).returns(nil).times(4)
+    @container.expects(:add_env_var).with('OPENSHIFT_MOCK_EXAMPLE_PUBLIC_PORT1', @ports_begin)
+    @container.expects(:add_env_var).with('LOAD_BALANCER_PORT', @ports_begin, true)
+    @container.expects(:add_env_var).with('OPENSHIFT_MOCK_EXAMPLE_PUBLIC_PORT2', @ports_begin+1)
+    @container.expects(:add_env_var).with('OPENSHIFT_MOCK_EXAMPLE_PUBLIC_PORT3', @ports_begin+2)
+    @container.expects(:add_env_var).with('OPENSHIFT_MOCK_EXAMPLE_PUBLIC_PORT4', @ports_begin+3)
 
     @container.create_public_endpoints(@mock_cartridge.name)
   end
@@ -335,5 +340,15 @@ class ApplicationContainerTest < OpenShift::NodeTestCase
     rc, msg = @container.user_var_add({'TOO_BIG' => '*' * 513})
     assert_equal 127, rc
     assert_equal "CLIENT_ERROR: 'TOO_BIG' value exceeds maximum size of 512b", msg
+  end
+
+  def test_deconfigure
+    @container.cartridge_model.expects(:deconfigure).with('mock-0.1')
+    @container.deconfigure('mock-0.1')
+  end
+
+  def test_unsubscribe
+    @container.cartridge_model.expects(:unsubscribe).with('mock-0.1', 'pub-cart')
+    @container.unsubscribe('mock-0.1', 'pub-cart')
   end
 end
